50751da0d3a3e32822c198c40517ae1113b88ffd
Michael Gerstenberger <eler@gmx.net>
2019-08-15 12:49:46 +0200

    changed website

--- bridge_server.py
+++ bridge_server.py
@@ -14,19 +14,11 @@ def print(txt):
 @route('/')
 def hello():
     site = """
-    import requests as req
-    import pickle
-            def get(URL = "http://167.71.35.239:8080/bridge"):
-                    data = req.get(URL)
-                    print(len(data.text))
-                    data = bytes(data.text,encoding='iso-8859-1')#data.text.encode()
-                    print(len(data))
-                    return pickle.loads(data)
-
-            def send(data, URL = "http://167.71.35.239:8080/bridge"):
-                    serialized = pickle.dumps(data, protocol=0)
-                    #print(serialized)
-                    req.post(URL, {"data":serialized},headers={'Content-Type': 'application/octet-stream'})"""
+    from bridge import *
+    import numpy as np
+    send(np.ones(100))
+    print(get())
+    """
     site = '<div style="white-space: pre-wrap;">' + site + '</div>'
     return site
 
