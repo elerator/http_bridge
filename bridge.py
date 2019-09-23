da513355c565f4f39300c422b9be6fc8483c3cbe
elerator <43006409+elerator@users.noreply.github.com>
2019-09-23 16:26:41 +0200

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -1,14 +1,13 @@
 import requests as req
 import pickle
 
-def get(URL = "http://167.71.35.239:8080/bridge"):
+#url = "http://167.71.35.239:8080/bridge"
+url = "http://algebrew.com/bridge"
+def get(URL = url):
 	data = req.get(URL)
-	print(len(data.text))
 	data = bytes(data.text,encoding='iso-8859-1')#data.text.encode()
-	print(len(data))
 	return pickle.loads(data)
 
-def send(data, URL = "http://167.71.35.239:8080/bridge"):
+def send(data, URL = url):
 	serialized = pickle.dumps(data, protocol=0)
-	#print(serialized)
 	req.post(URL, {"data":serialized},headers={'Content-Type': 'application/octet-stream'})
